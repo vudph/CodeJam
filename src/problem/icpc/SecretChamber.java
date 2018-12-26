55b7ee586e74b048e1f89b5c131a38d26f2d19fd
vudph <vu.dong@outlook.com>
2018-12-26 16:35:26 -0600

    Secret Chamber at Mount Rushmore

---
+++ src/problem/icpc/SecretChamber.java
@@ -0,0 +1,12 @@
+package problem.icpc;
+/*
+ * https://icpc.kattis.com/problems/secretchamber
+ */
+public class SecretChamber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
