0764acda230af140caebbfef9d64fa3da267a446
LAPTOP-OGA6UVAR\Vu Dong <hoangvudp@gmail.com>
2018-12-27 19:10:59 -0600

    Create KnuthMorrisPratt

---
+++ src/basic/string/KnuthMorrisPratt.java
@@ -0,0 +1,21 @@
+package basic.string;
+
+public class KnuthMorrisPratt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+/*
+Input:  text[] = "THIS IS A TEST TEXT"
+        pattern[] = "TEST"
+Output: Pattern found at index 10
+
+Input:  text[] =  "AABAACAADAABAABA"
+        pattern[] =  "AABA"
+Output: Pattern found at index 0
+        Pattern found at index 9
+        Pattern found at index 12
+*/
