a5785e23ada5c053a75bd8ce41325bf54141215c
vudph <vu.dong@outlook.com>
2019-01-03 16:26:53 -0600

    Update KMP

--- src/basic/string/KnuthMorrisPratt.java
+++ src/basic/string/KnuthMorrisPratt.java
@@ -1,21 +1,60 @@
 package basic.string;
 
+import java.util.Scanner;
+
+//http://www.personal.kent.edu/~rmuhamma/Algorithms/MyAlgorithms/StringMatch/kuthMP.htm
+
 public class KnuthMorrisPratt {
+	private static Scanner scanner = new Scanner(System.in);
+	private String T; // text
+	private String P; // pattern
+	private int S[]; //suffix array
+	
+	private void computeSuffixArray() {
+		S = new int[P.length() + 1];
+		S[0] = -1;
+		
+	}
+	
+	void computeLPSArray() {
+		// length of the previous longest prefix suffix
+		S = new int[P.length()];
+		S[0] = 0; // lps[0] is always 0
 
+		int j = 0;
+		int i = 1;
+		
+		// the loop calculates lps[i] for i = 1 to M-1
+		while (i < P.length()) {
+			if (P.charAt(i) == P.charAt(j)) {
+				j++;
+				i++;
+				S[i] = j;
+			} else { // (pat[i] != pat[len])
+				// This is tricky. Consider the example.
+				// AAACAAAA and i = 7. The idea is similar
+				// to search step.
+				if (j != 0) {
+					j = S[j - 1];
+
+					// Also, note that we do not increment
+					// i here
+				} else { // if (len == 0)
+					S[i] = 0;
+					i++;
+				}
+			}
+		}
+		for (i = 0; i < S.length; i++) {
+			System.out.print(S[i] + ",");
+		}
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		KnuthMorrisPratt kmp = new KnuthMorrisPratt();
+//		kmp.T = scanner.nextLine(); //abc abcdab abcdabcdabdabcdabdaef
+		kmp.P = scanner.nextLine(); // abcdabda  AAACAAAA
+		kmp.computeLPSArray();
 	}
 
 }
-/*
-Input:  text[] = "THIS IS A TEST TEXT"
-        pattern[] = "TEST"
-Output: Pattern found at index 10
-
-Input:  text[] =  "AABAACAADAABAABA"
-        pattern[] =  "AABA"
-Output: Pattern found at index 0
-        Pattern found at index 9
-        Pattern found at index 12
-*/
