dc93db4b1e0358473ce9bd31adb52276993fc9a9
vudph <vu.dong@outlook.com>
2019-01-04 16:57:29 -0600

    Update KMP

--- src/basic/string/KnuthMorrisPratt.java
+++ src/basic/string/KnuthMorrisPratt.java
@@ -6,54 +6,65 @@ import java.util.Scanner;
 //https://www.geeksforgeeks.org/kmp-algorithm-for-pattern-searching/
 public class KnuthMorrisPratt {
 	private static Scanner scanner = new Scanner(System.in);
-	private String T; // text
-	private String P; // pattern
-	private int S[]; //suffix array
 	
-	private void computeSuffixArray() {
-		S = new int[P.length() + 1];
-		S[0] = -1;
-		
+	private int search(String pattern, String text) {
+	    int[] lsp = computeLspTable(pattern);
+	    
+	    int j = 0;  // Number of chars matched in pattern
+	    for (int i = 0; i < text.length(); i++) {
+	        while (j > 0 && text.charAt(i) != pattern.charAt(j)) {
+	            // Fall back in the pattern
+	            j = lsp[j - 1];  // Strictly decreasing
+	        }
+	        if (text.charAt(i) == pattern.charAt(j)) {
+	            // Next char matched, increment position
+	            j++;
+	            if (j == pattern.length()) {
+	                int pos = i - (j - 1);
+	                System.out.println(pos);
+	                j = 0;
+	            }
+	            
+	        }
+	    }
+	    
+	    return -1;  // Not found
 	}
 	
-	void computeLPSArray() {
-		// length of the previous longest prefix suffix
-		S = new int[P.length()];
-		S[0] = 0; // lps[0] is always 0
-
-		int j = 0;
-		int i = 1;
-		
-		// the loop calculates lps[i] for i = 1 to M-1
-		while (i < P.length()) {
-			if (P.charAt(i) == P.charAt(j)) {
-				S[i] = j + 1;
-				i = i + 1;
-				j = j + 1;
-			} else { // (pat[i] != pat[len])
-				// This is tricky. Consider the example.
-				// AAACAAAA and i = 7. The idea is similar
-				// to search step.
-				if (j != 0) {
-					j = S[j - 1];
-					// Also, note that we do not increment
-					// i here
-				} else { // if (len == 0)
-					S[i] = 0;
-					i = i + 1;
-				}
+	private int[] computeLspTable(String pattern) { // the longest suffix-prefix
+		int lsp[] = new int[pattern.length()];
+		lsp[0] = 0;
+		for (int i = 1; i < pattern.length(); i++) {
+			int j = lsp[i - 1];
+			while (j > 0 && pattern.charAt(i) != pattern.charAt(j))
+				j = lsp[j - 1];
+			if (pattern.charAt(i) == pattern.charAt(j)) {
+				j++;
 			}
+			lsp[i] = j;
 		}
-		for (i = 0; i < S.length; i++) {
-			System.out.print(S[i] + ",");
-		}
+		return lsp;
+		
+//	    a a a a b
+//	    0 1 2 3 0
+	    
+//	    a b c a b c a c a  //abcdbcddd
+//	    0 0 0 1 2 3 4 0 1
+	    
+//	    a b c d b c d d d  //abcdbcddd
+//	    0 0 0 0 0 0 0 0 0
+//	    a b a b a b c
+//	    0 0 1 2 3 4 0
 	}
 	
 	public static void main(String[] args) {
 		KnuthMorrisPratt kmp = new KnuthMorrisPratt();
-//		kmp.T = scanner.nextLine(); //abc abcdab abcdabcdabdabcdabdaef
-		kmp.P = scanner.nextLine(); // abcdabda  AAACAAAA
-		kmp.computeLPSArray();
+		String text = scanner.nextLine(); //abc abcdab abcdabcdabdabcdabdaef
+		String pattern = scanner.nextLine(); // abcdabda  AAACAAAA
+		int pos = kmp.search(pattern, text);
+		System.out.println(pos);
+
+		
 	}
 
 }
