11a9230aecb2ce8ab3551beb340bb0a2fb5ffe3a
vudph <vu.dong@outlook.com>
2019-01-28 17:23:31 -0600

    Window sliding

---
+++ src/basic/windowsliding/MaxDifference.java
@@ -0,0 +1,38 @@
+package basic.windowsliding;
+
+/* Maximum difference between two elements such that larger element appears after the smaller number
+Given an array arr[] of integers, find out the maximum difference between any two elements such that larger element appears after the smaller number.
+Input : arr = {2, 3, 10, 6, 4, 8, 1}
+Output : 8
+Explanation : The maximum difference is between 10 and 2.
+
+Input : arr = {7, 9, 5, 6, 3, 2}
+Output : 2
+Explanation : The maximum difference is between 9 and 7.
+
+{2, 3, 10, 0, 4, 9, 1} => 9
+
+*/
+
+public class MaxDifference {
+	
+	private static int findMaxDiff(int a[]) {
+		int maxDiff = 0;
+		//brute force => O(n*(n-1))
+//		for (int i = 0; i < a.length - 1; i++) {
+//			int s = a[i];
+//			for (int j = i + 1; j < a.length; j++) {
+//				s = a[j] - a[i];
+//				maxDiff = Math.max(s, maxDiff);
+//			}
+//		}
+		// O(n)??
+		
+		return maxDiff;
+	}
+	
+	public static void main(String[] args) {
+		int a[] = {2, 3, 10, 0, 4, 9, 1};
+		System.out.println(findMaxDiff(a));
+	}
+}
---
+++ src/basic/windowsliding/MaxSum.java
@@ -0,0 +1,51 @@
+package basic.windowsliding;
+
+/*
+Given an array of integers of size ‘n’.
+Our aim is to calculate the maximum sum of ‘k’ 
+consecutive elements in the array.
+
+Input  : arr[] = {100, 200, 300, 400}
+         k = 2
+Output : 700
+
+Input  : arr[] = {1, 4, 2, 10, 23, 3, 1, 0, 20}
+         k = 4 
+Output : 39
+We get maximum sum by adding subarray {4, 2, 10, 23}
+of size 4.
+
+*/
+public class MaxSum {
+	
+	private static int findMaxSum(int a[], int k) {
+		if (k > a.length)
+			throw new IllegalArgumentException();
+		int maxSum = 0;
+		// brute force => O(n*k)
+//		for (int i = 0; i < a.length - k + 1; i++) {
+//			int sum = a[i];
+//			for (int j = i + 1; j < i + k; j++) {
+//				sum += a[j];
+//			}
+//			maxSum = Math.max(sum, maxSum);
+//		}
+		//window sliding => O(n)
+		for (int i = 0; i < k; i++) {
+			maxSum += a[i];
+		}
+		
+		int windowsSum = maxSum; 
+		for (int i = k; i < a.length; i++) {
+			windowsSum = (windowsSum + a[i]) - a[i - k];
+			maxSum = Math.max(windowsSum, maxSum);
+		}
+		
+		return maxSum;
+	}
+	public static void main(String[] args) {
+		int a[] = new int[] {1, 4, 2, 10, 23, 3, 1, 0, 20};
+		System.out.println(findMaxSum(a, 4));
+	}
+
+}
