e3fa1c815083c6d8b355f8d51b1b968b9eccc2e2
vudph <vu.dong@outlook.com>
2019-02-08 16:54:20 -0600

    Add

---
+++ src/leetcode/BusTicketCost.java
@@ -0,0 +1,37 @@
+package leetcode;
+/*
+https://leetcode.com/discuss/interview-question/124720/tickets-needed-to-get-minimum-cost
+The month has 30 days and there are three types of ticket:
+
+1-day ticket, costs 2, valid for one day;
+7-day ticket, costs 7, valid for seven consecutive days (e.g. if the first valid day is X, then the last valid day is X+6);
+30-day ticket, costs 25, valid for all thirty days of the month.
+You want to pay as little as possible.
+
+You are given a sorted (in increasing order) array A of dates when you will be travelling. For example, given:
+
+A[0] = 1
+A[1] = 2
+A[2] = 4
+A[3] = 5
+A[4] = 7
+A[5] = 29
+A[6] = 30
+
+You can buy one 7-day ticket and two 1-day tickets. The two 1-day tickets should be used on days 29 and 30.
+The 7-day ticket should be used on the first seven days of the month.
+The total cost is 11 and there is no possible way of paying less.
+ */
+public class BusTicketCost {
+
+	public int findCost(int a[]) {
+		
+		return -1;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
