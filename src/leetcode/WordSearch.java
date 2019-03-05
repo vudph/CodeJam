0c7b3b9cc598711c4facbff1444794f66befb31c
vudph <vu.dong@outlook.com>
2019-03-04 18:06:32 -0600

    Update

--- src/leetcode/WordSearch.java
+++ src/leetcode/WordSearch.java
@@ -21,20 +21,42 @@ Given word = "ABCB", return false.
 public class WordSearch {
 
 	public boolean exist(char[][] board, String word) {
-		int dx[] = {-1, 0, 1, 0};
-		int dy[] = {0, 1, 0, -1};
 		int n = board.length;
 		int m = board[0].length;
-		for (int i = 0; i < word.length(); i++) {
-			
-		}
+		char ch = word.charAt(0);
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
-				
+				if (board[i][j] == ch) {
+					boolean visited[][] = new boolean[n][m];
+					Boolean found[] = new Boolean[1];
+					found[0] = false;
+					dfs(board, n, m, word, i, j, visited, 1, found);
+					if (found[0])
+						return true;
+				}
 			}
 		}
 		return false;
     }
+	
+	private void dfs(char[][] board, int n, int m, String word, int x, int y, boolean visited[][], int idx, Boolean found[]) {
+		if (idx == word.length()) {
+			found[0] = true;
+			return;
+		}
+		visited[x][y] = true;
+		int dx[] = {-1, 0, 1, 0};
+		int dy[] = {0, 1, 0, -1};
+		for (int k = 0; k < 4; k++) {
+			int u = x + dx[k];
+			int v = y + dy[k];
+			if (isValidCell(u, v, n, m) && !visited[u][v] && board[u][v] == word.charAt(idx)) {
+				int id = idx + 1;
+				dfs(board, n, m, word, u, v, visited, id, found);
+				visited[u][v] = false;
+			}
+		}
+	}
 	
 	private boolean isValidCell(int x, int y, int n, int m) {
 		if (x < 0 || x >= n || y < 0 || y >= m)
@@ -43,8 +65,10 @@ public class WordSearch {
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		WordSearch ws = new WordSearch();
+		char board[][] = {{'A','B','C','E'}, {'S','F','C','S'}, {'A','D','E','E'}};
+		String word = "ABCB";
+		System.out.println(ws.exist(board, word));
 	}
 
 }
